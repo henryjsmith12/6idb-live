4a910d72bb3b4ae83d0c9e086c2b033a3ac4f19c
henryjsmith12 <henryjsmith12@outlook.com>
2022-07-05 15:35:29 -0500

    Remove "Add Parameter Dialog"

--- 6idb-live/source/parameter_widget.py
+++ 6idb-live/source/parameter_widget.py
@@ -14,14 +14,31 @@ class ParameterWidget(QtGui.QWidget):
         super(ParameterWidget, self).__init__(parent)
 
         # Widgets
+        self.parameter_name_lbl = QtGui.QLabel("Display Name:")
+        self.parameter_name_txt = QtGui.QLineEdit()
+        self.parameter_name_txt.setPlaceholderText("Delta")
+        self.parameter_pvname_lbl = QtGui.QLabel("PV Name:")
+        self.parameter_pvname_txt = QtGui.QLineEdit()
+        self.parameter_pvname_txt.setPlaceholderText("XXX:XX.XXX")
+        self.parameter_connected_lbl = QtGui.QLabel()
         self.add_parameter_btn = QtGui.QPushButton("Add Parameter")
+        self.add_parameter_btn.setAutoDefault(True)
+        self.add_list_btn = QtGui.QPushButton("Add List")
+        self.add_template_btn = QtGui.QPushButton("Add Template")
         self.parameter_tree = ParameterTree(showHeader=False)
 
         # Layout
         self.layout = QtGui.QGridLayout()
         self.setLayout(self.layout)
-        self.layout.addWidget(self.add_parameter_btn, 0, 0)
-        self.layout.addWidget(self.parameter_tree, 1, 0, 1, 2)
+        self.layout.addWidget(self.parameter_name_lbl, 0, 0, 1, 2)
+        self.layout.addWidget(self.parameter_name_txt, 0, 2, 1, 4)
+        self.layout.addWidget(self.parameter_pvname_lbl, 1, 0, 1, 2)
+        self.layout.addWidget(self.parameter_pvname_txt, 1, 2, 1, 4)
+        self.layout.addWidget(self.parameter_connected_lbl, 2, 0, 1, 2)
+        self.layout.addWidget(self.add_parameter_btn, 2, 2, 1, 4)
+        self.layout.addWidget(self.add_list_btn, 3, 0, 1, 3)
+        self.layout.addWidget(self.add_template_btn, 3, 3, 1, 3)
+        self.layout.addWidget(self.parameter_tree, 4, 0, 1, 6)
 
         # Signals
         self.add_parameter_btn.clicked.connect(self.add_parameter)
@@ -29,101 +46,35 @@ class ParameterWidget(QtGui.QWidget):
     # ------------------------------------------------------------------------------
 
     def add_parameter(self):
-        """
-        Opens dialog to add new parameter and creates parameter.
-        """
+        
+        display_name = self.parameter_name_txt.text()
+        pvname = self.parameter_pvname_txt.text()
 
-        dialog = AddParameterDialog()
-        dialog.exec_()
+        try:
+            pv = epics.PV(pvname)
+            if pv.connect():
+                parameter = PrimitiveParameter(
+                    display_name=display_name, 
+                    pvname=pvname
+                )
 
-        # Checks if dialog was accepted (with valid PV)
-        if dialog != QtGui.QDialogButtonBox.Ok:
+                # Adds parameter to widget
+                self.parameter_tree.addParameters(parameter)
 
-            # Creates parameter from PV name provided
-            parameter = Parameter(
-                display_name=dialog.display_name, 
-                pvname=dialog.pvname
-            )
+                self.parameter_name_txt.setText("")
+                self.parameter_pvname_txt.setText("")
+                self.parameter_connected_lbl.setText("")
 
-            # Adds parameter to widget
-            self.parameter_tree.addParameters(parameter)
-
-            #parameter.sigStateChanged.connect()
+                self.parameter_name_txt.setDefault(True)
+            else:
+                self.parameter_connected_lbl.setText("Not Connected")
+                self.parameter_connected_lbl.setStyleSheet("color: red")
+        except:
+            pass
             
 # ==================================================================================
 
-class AddParameterDialog(QtGui.QDialog):
-    """
-    Dialog that allows user to enter parameter information. Only valid PV names are 
-    accepted.
-    """
-
-    def __init__(self) -> None:
-        super(AddParameterDialog, self).__init__()
-
-        self.setModal(False)
-        self.connected = False
-        self.display_name = None
-        self.pvname = None
-
-        # Widgets
-        self.display_name_lbl = QtGui.QLabel("Display Name:")
-        self.display_name_txt = QtGui.QLineEdit()
-        self.display_name_txt.setPlaceholderText("Delta")
-        self.pvname_lbl = QtGui.QLabel("PV Name:")
-        self.pvname_txt = QtGui.QLineEdit()
-        self.pvname_txt.setPlaceholderText("XXX:XX.XXX")
-        self.connected_lbl = QtGui.QLabel()
-        self.dialog_btns = QtGui.QDialogButtonBox.Cancel | QtGui.QDialogButtonBox.Ok
-        self.dialog_btn_box = QtGui.QDialogButtonBox(self.dialog_btns)
-        self.dialog_btn_box.button(QtGui.QDialogButtonBox.Ok).setEnabled(False)
-
-        # Layout
-        self.layout = QtGui.QGridLayout()
-        self.setLayout(self.layout)
-        self.layout.addWidget(self.display_name_lbl, 0, 0)
-        self.layout.addWidget(self.display_name_txt, 0, 1)
-        self.layout.addWidget(self.pvname_lbl, 1, 0)
-        self.layout.addWidget(self.pvname_txt, 1, 1)
-        self.layout.addWidget(self.connected_lbl, 2, 0)
-        self.layout.addWidget(self.dialog_btn_box, 2, 1)
-        self.layout.setColumnStretch(0, 1)
-        self.layout.setColumnStretch(1, 1)
-
-        # Signals
-        self.pvname_txt.editingFinished.connect(self.validatePV)
-        self.dialog_btn_box.accepted.connect(self.accept)
-        self.dialog_btn_box.rejected.connect(self.reject)
-        
-    # ------------------------------------------------------------------------------
-
-    def validatePV(self):
-        """
-        Validates given PV name.
-        """
-
-        pvname = self.pvname_txt.text()
-
-        # Creates an EPICS PV from given PV name
-        pv = epics.PV(pvname)
-
-        # Checks if PV is valid
-        if pv.connect():
-            self.connected = True
-            self.display_name = self.display_name_txt.text()
-            self.pvname = pvname
-            self.connected_lbl.setText("Connected")
-            self.connected_lbl.setStyleSheet("color: green")
-            self.dialog_btn_box.button(QtGui.QDialogButtonBox.Ok).setEnabled(True)
-        else:
-            self.connected = False
-            self.connected_lbl.setText("Not Connected")
-            self.connected_lbl.setStyleSheet("color: red")
-            self.dialog_btn_box.button(QtGui.QDialogButtonBox.Ok).setEnabled(False)
-
-# ==================================================================================
-
-class Parameter(SimpleParameter):
+class PrimitiveParameter(SimpleParameter):
     """
     Parameter object for primitive types. Takes information from given PV.
     """
@@ -132,21 +83,19 @@ class Parameter(SimpleParameter):
         self.display_name = display_name
         self.pv = epics.PV(pvname)
         self.pvname = pvname
+        self.pvtype = type(self.pv.value)
         
-        value = self.pv.value
-        if type(value) == float:
-            value_type = "float"
-        elif type(value) == int:
-            value_type = "int"
-        elif type(value) == str:
-            value_type = "str"
+        if self.pvtype == float:
+            str_type = "float"
+        elif self.pvtype == int:
+            str_type = "int"
         else:
-            value_type = None
+            str_type = "str"
 
-        super(Parameter, self).__init__(
+        super(PrimitiveParameter, self).__init__(
             name=display_name,
-            value=value,
-            type=value_type,
+            value=self.pv.value,
+            type=str_type,
             suffix=self.pv.units,
             readonly=(not self.pv.write_access)
         )
@@ -164,7 +113,34 @@ class Parameter(SimpleParameter):
 
 # ==================================================================================
 
+class ImageParameter(SimpleParameter):
+    
+    def __init__(self, display_name : str, pvname : str) -> None:
+        self.display_name = display_name
+        self.pv = epics.PV(pvname)
+        self.pvname = pvname
+        self.image_array = self.pv.value
+        str_value = f"Image: ({self.image_array.shape})"
 
+        super(ImageParameter, self).__init__(
+            name=display_name,
+            value=str_value,
+            type="str",
+            readonly=True
+        )
+
+        epics.camonitor(pvname, callback=self.updateValue)
+
+    # ------------------------------------------------------------------------------
+
+    def updateValue(self, pvname=None, value=None, **kwargs):
+        """
+        Updates value as seen in the GUI
+        """
+
+        self.image_array = value
+
+# ==================================================================================
         
         
         
